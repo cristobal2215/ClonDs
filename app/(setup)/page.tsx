732f2e3b703e9bf0bf9f2622bdcc7847322707b4
cristobal2215 <cristobalroden@hotmail.com>
2023-09-16 16:18:53 -0300

    instalacion de react-form para los fomularios, plantilla de form, input y dialog y boton de crear server, 3 errores solucionados relacionados con el uso de los componentes (isMounted)

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage =async () => {
     const profile = await initialProfile();
@@ -16,7 +17,7 @@ const SetupPage =async () => {
     if (server) {
         return redirect(`/servers/${server.id}`);
     }
-    return <div>Create a server</div>;   
+    return <InitialModal />;   
 }
 
 export default SetupPage;
