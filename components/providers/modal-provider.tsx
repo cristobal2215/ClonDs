2a1e12db9318c21c236c61c45a979a90e3c21f22
cristobal2215 <cristobalroden@hotmail.com>
2023-10-06 21:35:34 -0300

    se añadio server sidebar, generar link de invitacion, server settings

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,6 +2,8 @@
 
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { useEffect, useState } from "react";
+import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -17,6 +19,8 @@ export const ModalProvider = () => {
     return (
         <>
           <CreateServerModal />
+          <InviteModal />
+          <EditServerModal />
         </>
       )
     }
