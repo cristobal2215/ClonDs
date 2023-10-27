055040f818d148ab163e6765c0119f87d058bc5d
cristobal2215 <cristobalroden@hotmail.com>
2023-10-27 14:29:31 -0300

    se agrego la funcion de leave server y delete server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -76,6 +76,7 @@ export const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                    onClick={() => onOpen("deleteServer", { server })}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                     >
                         Delete Server
@@ -84,6 +85,7 @@ export const ServerHeader = ({
                 )}
                 {!isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("leaveServer", { server })}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                     >
                         Leave Server
